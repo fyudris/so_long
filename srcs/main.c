4007d1de7190cde7850b915e2e43d241a8bd4fdc
Fanny Yudris <fanny.yudris@gmail.com>
2025-06-13 13:37:58 +0200

    feat: handle closing program using ESC key or clicking (x) on window

--- srcs/main.c
+++ srcs/main.c
@@ -6,42 +6,76 @@
 /*   By: fyudris <fyudris@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 17:33:00 by fyudris           #+#    #+#             */
-/*   Updated: 2025/06/13 13:26:05 by fyudris          ###   ########.fr       */
+/*   Updated: 2025/06/13 13:35:30 by fyudris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-// Define some constants for window size
+
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
 
-// We need a structure to hold our mlx pointers
+// Define keycodes for Linux (these may vary slightly)
+#define KEY_ESC 65307
+
 typedef struct s_data {
     void    *mlx_ptr;
     void    *win_ptr;
 }   t_data;
 
+/**
+ * @brief This function is called when a key is pressed.
+ *
+ * @param keycode The code of the key that was pressed.
+ * @param data A pointer to our custom data structure.
+ * @return int Always return 0.
+ */
+int handle_keypress(int keycode, t_data *data)
+{
+    if (keycode == KEY_ESC)
+    {
+        mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+        data->win_ptr = NULL; // It's good practice to NULL a freed pointer
+        exit(0); // A simple way to exit the program
+    }
+    return (0);
+}
+
+/**
+ * @brief This function is called when the red 'X' is clicked.
+ */
+int handle_close_button(t_data *data)
+{
+    mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+    data->win_ptr = NULL;
+    exit(0);
+    return (0);
+}
+
 int main(void)
 {
     t_data  data;
 
-    // 1. Initialize the library
     data.mlx_ptr = mlx_init();
-    if (data.mlx_ptr == NULL)
-        return (1); // Error
-
-    // 2. Create the window
-    data.win_ptr = mlx_new_window(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "My First Window!");
-    if (data.win_ptr == NULL)
-    {
-        // In case of error, you should clean up what was already created
+    if (!data.mlx_ptr)
         return (1);
-    }
+    data.win_ptr = mlx_new_window(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "Closable Window");
+    if (!data.win_ptr)
+        return (1);
 
-    // 3. Start the event loop
+    // --- NEW STUFF HERE ---
+    // Register the keypress handler
+    // mlx_key_hook is a simpler version of mlx_hook for key presses.
+    mlx_key_hook(data.win_ptr, handle_keypress, &data);
+
+    // Register the close button handler
+    // The event code '17' corresponds to the 'DestroyNotify' event.
+    // The mask '0' means no specific event mask.
+    mlx_hook(data.win_ptr, 17, 0, handle_close_button, &data);
+    // --- END OF NEW STUFF ---
+
     mlx_loop(data.mlx_ptr);
 
-    // The program will not reach here until the window is closed.
     return (0);
 }
